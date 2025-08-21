48d58f8e4e5cfd257c1b6e1e995e08543dd412d2
Hanjunhee-1 <hanjuni857@naver.com>
2025-08-21 13:38:50 +0900

    8393

---
+++ src/단계별로풀어보기/problem_8393.java
@@ -0,0 +1,15 @@
+package 단계별로풀어보기;
+
+import java.util.Scanner;
+
+public class problem_8393 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		
+		System.out.println((a*(a+1)) / 2);
+
+	}
+
+}
