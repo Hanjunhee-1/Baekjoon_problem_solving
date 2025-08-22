457cf9b71e885cb304e4adb2f71d377cf671b923
Hanjunhee-1 <hanjuni857@naver.com>
2025-08-22 14:07:21 +0900

    10951

---
+++ src/단계별로풀어보기/problem_10951.java
@@ -0,0 +1,20 @@
+package 단계별로풀어보기;
+
+import java.util.Scanner;
+
+public class problem_10951 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a, b;
+		
+		while (sc.hasNext()) {
+			a = sc.nextInt();
+			b = sc.nextInt();
+			
+			System.out.println(a+b);
+		}
+
+	}
+
+}
