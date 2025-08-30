f58a3b543c4cba668efbac5638e49f4de38f2e0a
Hanjunhee-1 <hanjuni857@naver.com>
2025-08-30 14:57:01 +0900

    2743

---
+++ src/단계별로풀어보기/problem_2743.java
@@ -0,0 +1,14 @@
+package 단계별로풀어보기;
+
+import java.util.Scanner;
+
+public class problem_2743 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String i = sc.next();
+		System.out.println(i.length());
+
+	}
+
+}
