571adad9cb753aebaa4a54e8b6f1aab974009bc8
Hanjunhee-1 <hanjuni857@naver.com>
2025-08-30 14:54:50 +0900

    27866

---
+++ src/단계별로풀어보기/problem_27866.java
@@ -0,0 +1,17 @@
+package 단계별로풀어보기;
+
+import java.util.Scanner;
+
+public class problem_27866 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String i = sc.next();
+		int n = sc.nextInt();
+		
+		System.out.println(i.charAt(n-1));
+
+	}
+
+}
