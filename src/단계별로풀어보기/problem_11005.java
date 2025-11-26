02d2ab8f045079f9d031ea3ef58518c897ef8d91
Hanjunhee-1 <hanjuni857@naver.com>
2025-11-26 18:19:49 +0900

    11005: 진법변환2

--- src/단계별로풀어보기/problem_11005.java
+++ src/단계별로풀어보기/problem_11005.java
@@ -12,10 +12,8 @@ public class problem_11005 {
 		/**
 		 * 60466175 36 -> ZZZZZ
 		 * 
-		 * N을 B로 계속해서 나눠야함.
+		 * N을 B로 계속해서 나눠주고 나머지 값을 알아야함.
 		 * 
-		 * 1. 총 몇 번 나눠지는지 구하기.
-		 * 2. (1) 의 결과를 통해서 N을 B로 나눈 몫을 B의 (1)제곱 수로 나눠서 값을 구하기.
 		 */
 		
 		// declare
@@ -25,31 +23,15 @@ public class problem_11005 {
 		int B = sc.nextInt();
 		String result = new String();
 		
-		// store the quotient of N divided by B in an array
-		while (N > B) {
-			arr.add(N);
+		// N을 B로 계속 나눠주면서 나머지값을 저장.
+		while (N >= B) {
+			arr.add(N%B);
 			N /= B;
-		} arr.add(N);
+		} arr.add(N%B);
 		
-		// calculate
 		for (int i=arr.size()-1; i>=0; i--) {
-			int exponent = arr.size() - 1 - i;
-			int n = 1;
-			
-			for (int j=0; j<exponent; j++) {
-				n *= B;
-			}
-			
-			int index = arr.get(i) / n;
-			
-			if (index < 10) {
-				result += index + "";
-			} else {
-				index += 55;
-				result += (char) index;
-			}
-			
-			System.out.println("i: " + i + ", arr.get(i): " + arr.get(i) + ", exponent: " + exponent + ", n: " + n + ", index: " + index + ", result: " + result);
+			if (arr.get(i) < 10) result += arr.get(i) + "";
+			else result += (char)(arr.get(i)+55);
 		}
 		
 		System.out.println(result);
