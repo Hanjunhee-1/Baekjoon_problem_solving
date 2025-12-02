666b84302376caa3578a34fc2be46aad7389a45a
Hanjunhee-1 <hanjuni857@naver.com>
2025-12-02 15:20:04 +0900

    27323: 직사각형

---
+++ src/단계별로풀어보기/problem_27323.java
@@ -0,0 +1,17 @@
+package 단계별로풀어보기;
+
+import java.util.Scanner;
+
+public class problem_27323 {
+
+	public static void main(String[] args) {
+		// 직사각형
+		// A와 B를 입력받아 각각 세로의 길이가 A이고 가로의 길이가 B인 직사각형의 넓이를 구하라.
+		
+		// declare
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextInt() * sc.nextInt());
+
+	}
+
+}
